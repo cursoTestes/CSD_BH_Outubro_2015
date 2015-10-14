30261eb2e91217a0e2be1817512dd85647236a12
cursoTestes <cfc@ufrj.br>
2015-10-14 17:25:01 -0300

    quebrando de proposito

--- SistemaVendas/testes/br/com/k21/UnitTestCalculadoraComissao.java
+++ SistemaVendas/testes/br/com/k21/UnitTestCalculadoraComissao.java
@@ -5,44 +5,44 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class UnitTestCalculadoraComissao {
-	
+
 	@Test
 	public void comissaoValorDeVendaMenorQue10000() {
 		double valorVenda = 1000.0;
 		double esperado = 50.0;
-	
+
 		double resultado = CalculadoraComissao.calcular(valorVenda);
-		
+
 		assertEquals(esperado, resultado, 0);
 	}
-	
+
 	@Test
 	public void comissaoValorDeVendaIgual10000() {
 		double valorVenda = 10000.0;
-		double esperado = 500.0;
-	
+		double esperado = 501.0;
+
 		double resultado = CalculadoraComissao.calcular(valorVenda);
-		
+
 		assertEquals(esperado, resultado, 0);
 	}
-	
+
 	@Test
 	public void comissaoValorDeVendaMaiorQue10000() {
 		double valorVenda = 20000.0;
 		double esperado = 1200.0;
-	
+
 		double resultado = CalculadoraComissao.calcular(valorVenda);
-		
+
 		assertEquals(esperado, resultado, 0);
 	}
-	
+
 	@Test
 	public void comissaoEmpresaSemPrejuizo() {
 		double valorVenda = 55.59;
 		double esperado = 2.77;
-	
+
 		double resultado = CalculadoraComissao.calcular(valorVenda);
-		
+
 		assertEquals(esperado, resultado, 0);
 	}
 
